f0c9563a38df1a4662d037bbc0364e7ad61c3b80
psx95 <pranav.ps95@hotmail.com>
2018-12-04 00:35:34 +0530

    feat(AboutScreen): Adds the about screen to the game

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Pranav Sharma on 04/12/18.
+//  Copyright © 2018 Pranav Sharma. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
