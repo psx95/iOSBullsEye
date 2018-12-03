51abdb48e5aeea07c003e1c9518533d7734d7ccc
psx95 <pranav.ps95@hotmail.com>
2018-12-04 00:51:06 +0530

    feat(About): Adds the about the author page

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Pranav Sharma on 04/12/18.
+//  Copyright © 2018 Pranav Sharma. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func back() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
